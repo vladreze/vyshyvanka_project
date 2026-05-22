3c502ce9ac699a2e38f32673990281a9b66a0e0d
vladreze <vladrezanov07@gmail.com>
2026-05-22 13:36:09 +0300

    feat: add ToolBarPanel for user interaction with color palette and symmetry options

---
+++ src/main/java/View/ToolBarPanel.java
@@ -0,0 +1,70 @@
+package View;
+
+import Controller.AppController;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
+public class ToolBarPanel extends VBox{
+    private final EmbroideryCanvas canvas;
+    private final AppController controller;
+
+    public ToolBarPanel(EmbroideryCanvas canvas, AppController controller) {
+        this.canvas = canvas;
+        this.controller = controller;
+
+        setPadding(new Insets(10));
+        setSpacing(10);
+        this.getStyleClass().add("toolbar-panel");
+
+        initUI();
+    }
+
+    private void initUI() {
+
+        getChildren().add(new Label("Color Palette"));
+        ColorPicker colorPicker = new ColorPicker();
+        colorPicker.setValue(Color.BLACK);
+        colorPicker.setMaxWidth(Double.MAX_VALUE);
+        colorPicker.setOnAction(c ->{
+            Color fxcolor = colorPicker.getValue();
+            String hexColor = String.format("%02X%02X%02X",
+                    (int) (fxcolor.getRed() * 255),
+                    (int) (fxcolor.getGreen() * 255),
+                    (int) (fxcolor.getBlue() * 255));
+            controller.setCurrentColor(hexColor);
+        });
+        getChildren().add(colorPicker);
+
+        getChildren().add(new Label("Toolbar"));
+
+        Button eraseButton = new Button("Erase");
+        eraseButton.setMaxWidth(Double.MAX_VALUE);
+        eraseButton.getStyleClass().add("tool-button");
+        eraseButton.setOnAction(e -> { controller.setCurrentColor(null);
+        });
+        getChildren().add(eraseButton);
+
+        Button clearAllButton = new Button("Clear All");
+        clearAllButton.setMaxWidth(Double.MAX_VALUE);
+        clearAllButton.getStyleClass().add("tool-button");
+        clearAllButton.setOnAction(c ->{
+            controller.clearCanvas();
+            canvas.redraw();
+        });
+        getChildren().add(clearAllButton);
+
+        getChildren().add(new Label("Symmetry:"));
+        CheckBox horizontalSymmetryCheckBox = new CheckBox("Horizontal");
+        horizontalSymmetryCheckBox.setOnAction(h -> controller.setHorizontalSymmetry(horizontalSymmetryCheckBox.isSelected()));
+        getChildren().add(horizontalSymmetryCheckBox);
+
+        CheckBox verticalSymmetryCheckBox = new CheckBox("Vertical");
+        verticalSymmetryCheckBox.setOnAction(v -> controller.setVerticalSymmetry(verticalSymmetryCheckBox.isSelected()));
+        getChildren().add(verticalSymmetryCheckBox);
+    }
+}
