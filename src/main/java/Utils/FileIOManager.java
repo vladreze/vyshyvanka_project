4d984ff942593078a24d5af7e5718d2b1570eaf4
vladreze <vladrezanov07@gmail.com>
2026-05-22 13:47:09 +0300

    feat: add FileIOManager for saving canvas as PNG

---
+++ src/main/java/Utils/FileIOManager.java
@@ -0,0 +1,40 @@
+package Utils;
+
+import com.sun.javafx.embed.swing.SwingFXUtilsImpl;
+import javafx.embed.swing.SwingFXUtils;
+import javafx.scene.image.WritableImage;
+import javafx.stage.FileChooser;
+
+import java.awt.*;
+import java.io.File;
+
+import javafx.scene.canvas.Canvas;
+import javafx.stage.Window;
+
+import javax.imageio.ImageIO;
+
+public class FileIOManager {
+
+    public static void saveCanvasToPng(Canvas canvas, Window window) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Save Canvas as PNG");
+
+        FileChooser.ExtensionFilter extensionFilter = new FileChooser.ExtensionFilter("PNG files (*.png)", "*.png");
+        fileChooser.getExtensionFilters().add(extensionFilter);
+
+        File file = fileChooser.showSaveDialog(window);
+
+        if(file != null){
+            try {
+                WritableImage image = new WritableImage((int) canvas.getWidth(), (int) canvas.getHeight());
+                canvas.snapshot(null, image);
+
+                ImageIO.write(SwingFXUtils.fromFXImage(image, null), "png", file);
+                System.out.println("Canvas saved successfully to " + file.getAbsolutePath());
+
+            } catch (Exception e) {
+                System.out.println("Error saving canvas: " + e.getMessage());
+            }
+        }
+    }
+}
