b62b307cb8ad094d7d817c94f1a35b1fd0656d4f
vladreze <vladrezanov07@gmail.com>
2026-05-23 17:43:23 +0300

    feat: add AboutProject class and integrate About button in ToolBarPanel for project information display

---
+++ src/main/java/AdditionalInformation/AboutProject.java
@@ -0,0 +1,49 @@
+/*
+ * This class creates a pop-up window that provides information about the project, including the project name, author, and description. It also includes a button to close the pop-up.
+ */
+
+
+
+package AdditionalInformation;
+
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+
+
+public class AboutProject {
+
+    public static void displayInfo(){
+        Stage popUpStage = new Stage();
+        VBox popUpLayout = new VBox();
+        popUpLayout.setPadding(new Insets(15));
+        popUpLayout.setAlignment(Pos.CENTER);
+        popUpLayout.getChildren().add(new Label("Project: Embroidery Design App"));
+        popUpLayout.getChildren().add(new Label("Author: Vladyslav Rezanov"));
+        popUpLayout.getChildren().add(new Label("Description: A JavaFX application for creating pixel art embroidery designs."));
+        Scene popUpScene = new Scene(popUpLayout, 750, 350);
+        popUpStage.setScene(popUpScene);
+        popUpStage.show();
+
+
+        popUpLayout.getChildren().add(new Label(""));
+        popUpLayout.setMinHeight(200);
+        Button closeButton = new Button("Understood");
+        closeButton.setMaxWidth(100);
+        closeButton.setOnAction(e -> popUpStage.close());
+        closeButton.getStyleClass().add("tool-button");
+        popUpLayout.getChildren().add(closeButton);
+
+
+        try {
+            String css = AboutProject.class.getResource("/styles.css").toExternalForm();
+            popUpScene.getStylesheets().add(css);
+        } catch (Exception e) {
+            System.out.println("Error loading CSS" + e.getMessage());
+        }
+    }
+}
--- src/main/java/View/ToolBarPanel.java
+++ src/main/java/View/ToolBarPanel.java
@@ -1,14 +1,17 @@
 package View;
 
+import AdditionalInformation.AboutProject;
 import Controller.AppController;
 import Utils.FileIOManager;
 import javafx.geometry.Insets;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ColorPicker;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 
 public class ToolBarPanel extends VBox{
     private final EmbroideryCanvas canvas;
@@ -17,9 +20,8 @@ public class ToolBarPanel extends VBox{
     public ToolBarPanel(EmbroideryCanvas canvas, AppController controller) {
         this.canvas = canvas;
         this.controller = controller;
-
-        setPadding(new Insets(10));
         setSpacing(10);
+        setPadding(new Insets(10));
         this.getStyleClass().add("toolbar-panel");
 
         initUI();
@@ -27,6 +29,7 @@ public class ToolBarPanel extends VBox{
 
     private void initUI() {
 
+        // Color Palette
         getChildren().add(new Label("Color Palette"));
         ColorPicker colorPicker = new ColorPicker();
         colorPicker.setValue(Color.BLACK);
@@ -43,6 +46,7 @@ public class ToolBarPanel extends VBox{
 
         getChildren().add(new Label("Toolbar"));
 
+        // Erase Button
         Button eraseButton = new Button("Erase");
         eraseButton.setMaxWidth(Double.MAX_VALUE);
         eraseButton.getStyleClass().add("tool-button");
@@ -50,6 +54,7 @@ public class ToolBarPanel extends VBox{
         });
         getChildren().add(eraseButton);
 
+        // Clear All Button
         Button clearAllButton = new Button("Clear All");
         clearAllButton.setMaxWidth(Double.MAX_VALUE);
         clearAllButton.getStyleClass().add("tool-button");
@@ -60,14 +65,17 @@ public class ToolBarPanel extends VBox{
         getChildren().add(clearAllButton);
 
         getChildren().add(new Label("Symmetry:"));
+        // Horizontal Symmetry Checkbox
         CheckBox horizontalSymmetryCheckBox = new CheckBox("Horizontal");
         horizontalSymmetryCheckBox.setOnAction(h -> controller.setHorizontalSymmetry(horizontalSymmetryCheckBox.isSelected()));
         getChildren().add(horizontalSymmetryCheckBox);
 
+        // Vertical Symmetry Checkbox
         CheckBox verticalSymmetryCheckBox = new CheckBox("Vertical");
         verticalSymmetryCheckBox.setOnAction(v -> controller.setVerticalSymmetry(verticalSymmetryCheckBox.isSelected()));
         getChildren().add(verticalSymmetryCheckBox);
 
+        // Export Button
         Button exportButton = new Button("Export as PNG");
         exportButton.setMaxWidth(Double.MAX_VALUE);
         exportButton.getStyleClass().add("tool-button");
@@ -75,6 +83,21 @@ public class ToolBarPanel extends VBox{
             FileIOManager.saveCanvasToPng(canvas, canvas.getScene().getWindow());
         });
         getChildren().add(exportButton);
+
+        Label space = new Label("");
+        space.setMinHeight(200);
+        getChildren().add(space);
+
+        // About Button
+        Button aboutButton = new Button("About");
+        aboutButton.setMaxWidth(Double.MAX_VALUE);
+        aboutButton.getStyleClass().add("tool-button");
+        aboutButton.setOnAction(e -> {
+            AboutProject.displayInfo();
+        });
+        getChildren().add(aboutButton);
+
+
 
     }
 }
